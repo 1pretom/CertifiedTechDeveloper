0f8de143d8e6a2adf293124685f3b832867cffb9
Washington Damacena <washingtonldamacenac@gmail.com>
2022-09-03 15:21:06 -0300

    att main

--- Programacao-Orientada-a-Objetos/Checkpoint1/Checkpoint-1/src/ApresentacaoTcc/Main.java
+++ Programacao-Orientada-a-Objetos/Checkpoint1/Checkpoint-1/src/ApresentacaoTcc/Main.java
@@ -5,10 +5,10 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
 
-    Aluno aluno1 = new Aluno("Gabrielle ", "Araujo", "Abordade sobre a gravidez na adolescencia", true);
-    Aluno aluno2 = new Aluno("Julia", "Carvalho", "Diagnósticos de Enfermagem identificados em Unidade de Terapia Intensiva Neonatal:", true);
-    Aluno aluno3 = new Aluno("Washington", "Damacena","Estratégias educativas em saúde para reduzir a incidência de gravidez na adolescencia", false);
-    aluno3.convidarPessoas(5);
+    Aluno aluno1 = new Aluno("Gabrielle ", "Araujo ", "Abordade sobre a gravidez na adolescencia", true);
+    Aluno aluno2 = new Aluno("Julia ", "Carvalho ", "Diagnósticos de Enfermagem identificados em Unidade de Terapia Intensiva Neonatal:", true);
+    Aluno aluno3 = new Aluno("Washington ", "Damacena ","Estratégias educativas em saúde para reduzir a incidência de gravidez na adolescencia", false);
+        System.out.println("A quantidade de convidados de " + aluno3.getNome() + aluno3.getSobrenome() + " é: " + aluno3.convidarPessoas(5));
 
     Professor professor1 = new Professor("Diana ", "Salete", "Saude da mulher", false);
     Professor professor2 = new Professor("Aline ", "Gomes", "Introdução a reprodução humana", true);
