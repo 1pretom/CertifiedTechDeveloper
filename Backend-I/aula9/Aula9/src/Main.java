4f3842c62f88e66c0d4405f7da9ff41da438ffd2
Washington Damacena <washingtonldamacenac@gmail.com>
2022-11-16 21:51:25 -0300

    aulas 9 e 8

---
+++ Backend-I/aula9/Aula9/src/Main.java
@@ -0,0 +1,67 @@
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    //método logger com static final para indicar que não vai mudar
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    //método static final para o banco de dados, variáveis constantes usamos em maiúsculo
+    //criando o banco de dados como no MYSQL
+    private static final String SQLCREATETABLE = "DROP TABLE IF EXISTS Animal;"+
+            "CREATE TABLE Animal"+
+            "(id INT PRIMARY KEY, "+
+            "nome VARCHAR(100) NOT NULL, "+
+            "tipo VARCHAR(50) NOT NULL);";
+
+    //criando animais para colocar no banco de dados
+    private static final String SQLINSERT1 = "INSERT INTO Animal (id, nome, tipo) VALUES"+
+            "(1,'Meg', 'Cachorro);";
+
+    private static final String SQLINSERT2 = "INSERT INTO Animal (id, nome, tipo) VALUES"+
+            "(2,'Mingau', 'Gato);";
+
+    private static final String SQLINSERT3 = "INSERT INTO Animal (id, nome, tipo) VALUES"+
+            "(3,'Dumbo', 'Elefante);";
+
+    private static final String SQLINSERT4 = "INSERT INTO Animal (id, nome, tipo) VALUES"+
+            "(4,'Pup', 'Cachorro);";
+
+    private static final String SQLINSERT5 = "INSERT INTO Animal (id, nome, tipo) VALUES"+
+            "(5,'Pé de pano', 'Cavalo);";
+
+    //criando comando para deletar algo da tabela
+    private static final String SQLDELETE = "DELETE FROM Animal WHERE id = 1;";
+
+    //criando comando para selecionar os animais
+    private static final String SQLSELECT = "SELECT * FROM Animal;";
+
+    //criando uma conexão com o banco de dados h2
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+
+        try {
+            logger.info("Abrindo conexão com o banco de dados");
+            connection = getConnection();
+            Statement statement = connection.createStatement(); //criando statement para fazer criações e inserções no Banco de Dados
+            logger.info("Criando tabela no banco de dados");
+            statement.execute(SQLCREATETABLE);//para executar a base de dados criada
+        }catch (Exception e){
+            logger.error("Erro ao executar SQL");
+        }finally {//para encerrar a aplicação
+            if (connection == null)
+                return;
+            logger.info("Fechando conexão com o banco de dados");
+            connection.close();
+        }
+    }
+
+    //abrindo a conexão
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {//exceções que utiliza
+        Class.forName("org.h2.Driver");//passando o "driver" traduz para o banco de dados
+        return DriverManager.getConnection("jdbc:h2:~/animal","sa","");//passando os parametros de como ele vai conectar
+    }
+}
